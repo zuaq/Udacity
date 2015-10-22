f6b4018bac362ed012003ca7432e9b9aaaab014e
BZroid <bodyzlam@gmail.com>
2015-10-22 11:35:59 +0700

    Udacity Learning
    This is my Udacity with my code

---
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.android.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    int quantity;
+    TextView quantityTextView;
+    Button btn1;
+    int costPerCup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        btn1 = (Button) findViewById(R.id.decrement);
+        btn1.setEnabled(false);
+        quantity = 0;
+        costPerCup = 5;
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+
+        int price = calculatePrice();
+        displayMessage(createOrderSummary(price));
+
+    }
+
+    private String createOrderSummary(int price) {
+        String priceMessage = "Name: Benz";
+        priceMessage += "\nQuantity : " + quantity;
+        priceMessage += "\nTotal: $" + price;
+        priceMessage += "\nThank you!";
+        return priceMessage;
+    }
+
+    public void increment(View view) {
+        quantity++;
+        displayQuantity(quantity);
+    }
+
+    public void decrement(View view) {
+        if (quantity > 0) {
+            quantity--;
+            displayQuantity(quantity);
+
+        } else {
+            btn1.setEnabled(false);
+        }
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayQuantity(int number) {
+
+        quantityTextView.setText(number);
+        btn1.setEnabled(true);
+    }
+
+    /**
+     * Calculates the price of the order based on the current quantity.
+     *
+     * @return the price
+     */
+    private int calculatePrice() {
+        return quantity * costPerCup;
+    }
+
+
+}
---
+++ PracticeSet2/app/src/main/java/com/example/android/practiceset2/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.android.practiceset2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // PASTE CODE YOU WANT TO TEST HERE
+
+        /* first test
+        int raspberryPrice = 5;
+        display1("1 box: $" + raspberryPrice);
+        raspberryPrice = 10;
+        display2("2 boxes: $" + (raspberryPrice));
+        display3("3 boxes: $" + (raspberryPrice * 3));*/
+
+        /*second test
+        int weekday = 5;
+        int weekend = 9;
+        int optimalHours = 7 * 8;
+
+        int actualHours = weekday;
+        actualHours = actualHours + weekend * 2;
+        int solution = optimalHours - actualHours;
+        display(solution);*/
+
+        /* third
+        int day1 = 15;
+        int day2 = 22;
+        int day3 = 18;
+        display(day1 + day2 + day3 / 3);*/
+
+        /*forth
+        int raspberryPrice = 5;
+        display1("1 box: $" + raspberryPrice);
+        display2("2 boxes: $" + (raspberryPrice * 2));
+        display3("3 boxes: $" + (raspberryPrice * 3));
+
+        int raspberryPrice = 5;
+        display1("1 box: $" + raspberryPrice);
+        raspberryPrice = 10;
+        display2("2 boxes: $" + (raspberryPrice));
+        display3("3 boxes: $" + (raspberryPrice * 3));*/
+
+        /*
+        int dollars = 40;
+        int dollarsToYen = 119;
+        int yen = dollarsToYen * dollars;
+        display(yen);*/
+
+        String firstName = "Lyla";
+        String lastName = "Fujiwara";
+        String contactInfo = firstName + " " + lastName;
+        contactInfo = "<" + lastName + "." + firstName + "@justjava.com>";
+        display(contactInfo);
+    }
+
+    /**
+     * Display methods that allow the text to appear on the screen. Don't worry if you don't know
+     * how these work yet. We'll be covering them in lesson 3.
+     */
+
+    public void display(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(text);
+    }
+
+    public void display(int text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(text + "");
+    }
+
+    public void display1(String text) {
+        display(text);
+    }
+
+    public void display2(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view_2);
+        t.setText(text);
+    }
+
+    public void display3(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view_3);
+        t.setText(text);
+    }
+}
